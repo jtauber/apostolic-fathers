4177d3b898e13150d7ef2b2a5b89e0a3bcb74925
James Tauber <jtauber@jtauber.com>
2018-10-21 04:30:22 -0400

    updated scripts

--- scripts/extract.py
+++ scripts/extract.py
@@ -6,7 +6,7 @@
 from itertools import zip_longest
 from unicodedata import normalize
 
-WORK = "001-i_clement"
+WORK = "002-ii_clement"
 
 LINES_1 = open(f"comparison/{WORK}_COMPARE_JT2.txt").readlines()
 LINES_2 = open(f"comparison/{WORK}_COMPARE_SM2.txt").readlines()
