bea2fc0356678988e3d63128a0d4cae2c728400c
James Tauber <jtauber@jtauber.com>
2019-01-29 13:47:43 -0500

    improvements to check script

--- scripts/check.py
+++ scripts/check.py
@@ -6,9 +6,9 @@ import sys
 import unicodedata
 
 
-ELLIPSIS = r"\.\.\."
-LATIN = r"[A-Z]?[a-zë]+[\u002C\u002E\u003B\u003F]?"
-NUMBER = r"[βιη]+[\u02B9]"
+ELLIPSIS = r"\u2026"
+LATIN = r"[A-Za-zë]+[\u002C\u002E\u003A\u003B\u003F]?"
+NUMBER = r"[αβγδεϛζηθικλμνξοπϟρστυφχψωϡ]+[\u02B9]"
 PUNC = r"[\u002C\u002E\u003B\u00B7]"
 GRCHAR = r"[\u0374\u0390-\u03A1\u03A3-\u03C1\u03C3-\u03CE" \
          r"\u1F00-\u1F15\u1F18-\u1F1D\u1F20-\u1F45\u1F48-\u1F4D\u1F50-\u1F57" \
@@ -17,11 +17,11 @@ GRCHAR = r"[\u0374\u0390-\u03A1\u03A3-\u03C1\u03C3-\u03CE" \
          r"\u1FF2-\u1FF4\u1FF6-\u1FFC]"
 
 WORD_REGEX = fr"({ELLIPSIS}|{LATIN}|{NUMBER}{PUNC}?|" \
-             fr"\(?{GRCHAR}+\u03C2?\)?[\u02BC]?{PUNC}?\)?\*?)$"
+             fr"\(?{GRCHAR}+\u03C2?\)?[\u2019]?{PUNC}?\)?\*?)$"
 
 
 def error(*parts):
-    print(": ".join(parts), file=sys.stderr)
+    print(": ".join(map(str, parts)), file=sys.stderr)
 
 
 for fname in glob.glob("structured/*_CORRECTED.txt"):
@@ -44,8 +44,9 @@ for fname in glob.glob("structured/*_CORRECTED.txt"):
                 error(fname, lnum, "BAD WHITESPACE")
 
             # the first token must be a reference of the form <num>.<num>
+            # or <num>.<num>.<num>
 
-            if not re.match(r"\d+\.\d+$", parts[0]):
+            if not re.match(r"\d+\.\d+(\.\d+)?$", parts[0]):
                 error(fname, lnum, "BAD REFERENCE FORM")
 
             # references must be in sort order
@@ -53,15 +54,15 @@ for fname in glob.glob("structured/*_CORRECTED.txt"):
             ref = tuple(int(i) for i in parts[0].split("."))
 
             if prev_ref and ref <= prev_ref:
-                error(fname, lnum, "BAD REFERENCE ORDERING")
+                error(fname, lnum, f"BAD REFERENCE ORDERING")
 
             prev_ref = ref
 
             for word in parts[1:]:
 
-                # word must be NFKC normalized
+                # word must be NFC normalized
 
-                if word != unicodedata.normalize("NFKC", word):
+                if word != unicodedata.normalize("NFC", word):
                     error(fname, lnum, word, "BAD UNICODE NORMALIZATION")
 
                 # word must contain value sequence of Greek characters
@@ -69,11 +70,15 @@ for fname in glob.glob("structured/*_CORRECTED.txt"):
                 if not re.match(WORD_REGEX, word):
                     error(fname, lnum, word, "BAD WORD")
                     for ch in word:
-                        print(ch, hex(ord(ch)), unicodedata.name(ch))
+                        print("\t", ch, hex(ord(ch)), unicodedata.name(ch))
 
-                # breathing must be correct (if not number or latin)
+                # breathing must be correct (if not number or latin or uppercase)
 
-                if not re.match(NUMBER, word) and not re.match(LATIN, word):
+                if (
+                    not re.match(NUMBER, word) and
+                    not re.match(LATIN, word) and
+                    not word == word.upper()
+                ):
                     d = unicodedata.normalize("NFD", word.lower())
                     if d[0] in "αεηιοω":
                         if d[1] in "\u0313\u0314":
